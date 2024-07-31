115bc3b2be9c974950aacf98cfc6e7098c1706cf
Maciej Kusiak <maciekkusiak27@gmail.com>
2024-07-31 23:25:08 +0200

    prototype photos

--- js/script.js
+++ js/script.js
@@ -33,3 +33,57 @@ function showSection(sectionId) {
       menuIcon.classList.add("fa-info-circle");
     }
   }
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const experienceLink = document.querySelector("a[onclick=\"showSection('experience')\"]");
+    const hobbysLink = document.querySelector("a[onclick=\"showSection('hobbys')\"]");
+    const webPagesLink = document.querySelector("a[onclick=\"showSection('web-pages')\"]");
+    const educationLink = document.querySelector("a[onclick=\"showSection('education')\"]");
+    const coursesLink = document.querySelector("a[onclick=\"showSection('courses')\"]");
+    const projectsLink = document.querySelector("a[onclick=\"showSection('projects')\"]");
+    const profilePhoto = document.getElementById("profile-photo");
+  
+    experienceLink.addEventListener("mouseover", function() {
+      profilePhoto.src = "/assets/images/me/gora.png";
+    });
+  
+    experienceLink.addEventListener("mouseout", function() {
+      profilePhoto.src = "/assets/images/me/front.png";
+    });
+  
+    hobbysLink.addEventListener("mouseover", function() {
+      profilePhoto.src = "/assets/images/me/dol.png";
+    });
+  
+    hobbysLink.addEventListener("mouseout", function() {
+      profilePhoto.src = "/assets/images/me/front.png";
+    });
+
+    webPagesLink.addEventListener("mouseover", function() {
+      profilePhoto.src = "/assets/images/me/lgora.png";
+    });  
+    webPagesLink.addEventListener("mouseout", function() {
+      profilePhoto.src = "/assets/images/me/front.png";
+    });
+
+    educationLink.addEventListener("mouseover", function() {
+      profilePhoto.src = "/assets/images/me/ldol.png";
+    });  
+    educationLink.addEventListener("mouseout", function() {
+      profilePhoto.src = "/assets/images/me/front.png";
+    });
+
+    projectsLink.addEventListener("mouseover", function() {
+      profilePhoto.src = "/assets/images/me/rgora.png";
+    });  
+    projectsLink.addEventListener("mouseout", function() {
+      profilePhoto.src = "/assets/images/me/front.png";
+    });
+
+    coursesLink.addEventListener("mouseover", function() {
+      profilePhoto.src = "/assets/images/me/rdol.png";
+    });  
+    coursesLink.addEventListener("mouseout", function() {
+      profilePhoto.src = "/assets/images/me/front.png";
+    });
+  });
