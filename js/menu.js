1c6e51a8a46b11c06373fce254ebf2459dd748b8
Maciej Kusiak <maciekkusiak27@gmail.com>
2024-09-04 12:56:45 +0200

    document height fix and mobile fix

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,10 @@
 export function toggleMenu() {
-    const menu = document.querySelector(".header-container .info");
-    const menuIcon = document.querySelector(".menu-icon i");
+  const menu = document.querySelector(".header-container .info");
+  const menuIcon = document.querySelector(".menu-icon i");
+  
+  if (menu) {
     menu.classList.toggle("active");
+    
     if (menu.classList.contains("active")) {
       menuIcon.classList.remove("fa-info-circle");
       menuIcon.classList.add("fa-times");
@@ -10,3 +13,12 @@ export function toggleMenu() {
       menuIcon.classList.add("fa-info-circle");
     }
   }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuIcon = document.querySelector(".menu-icon i");
+  
+  if (menuIcon) {
+    menuIcon.addEventListener("click", toggleMenu);
+  }
+});
